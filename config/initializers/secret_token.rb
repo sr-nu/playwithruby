0b741c27944803ebf68fa12862d769cc026b8813
Srikanth <srikanth.nutigattu@gmail.com>
2012-05-13 16:30:35 +0530

    Barebones Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playwithruby::Application.config.secret_token = '081ea175d63bb8d49dcdc75ccf618f9f6b8d1ab289f49d08b84ac39379b987a145a1422c11fab0104f629d26568c7a859ff752507ffa10a41c7bce52e9713d7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Playwithruby::Application.config.session_store :cookie_store, :key => '_playwithruby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Playwithruby::Application.config.session_store :active_record_store

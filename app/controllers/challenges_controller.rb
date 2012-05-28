8c37ae20eec69b6e86d419ae7846ae5fcb1ee0f6
Abhinaya <sabhinay@thoughtworks.com>
2012-05-28 10:25:05 +0530

    Abhi- Removed unwanted code and added counter

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,79 +1,22 @@
 class ChallengesController < ApplicationController
   # GET /challenges
   # GET /challenges.json
+  @@count = 1
 
-  @@counter = 1
-  def index
-    @challenges = Challenge.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @challenges }
-    end
-  end
-
-  # GET /challenges/1
-  # GET /challenges/1.json
-  def show
-    @challenge = Challenge.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @challenge }
-    end
-  end
-
-  # GET /challenges/new
-  # GET /challenges/new.json
   def new
+    @count = @@count
     @challenge = Challenge.new
     @questionaire ||= Questionaire.first
   end
 
-  # GET /challenges/1/edit
-  def edit
-    @challenge = Challenge.find(params[:id])
-  end
-
-  # POST /challenges
-  # POST /challenges.json
   def create
-    @@counter += 1
     @questionaire = Questionaire.where("id > (?)",params["challenge"]["questionaire_id"]).first
-    p 's'*100
-    p @@counter
-    @@counter >= 10 ? render(:display_result) : redirect_to(new_challenge_path)
+    @@count = @@count + 1
+    @count = @@count
+    @@count > 10 ? render(:display_result) : redirect_to(new_challenge_path)
   end
 
   def display_result
-  end
-
-  # PUT /challenges/1
-  # PUT /challenges/1.json
-  def update
-    @challenge = Challenge.find(params[:id])
-
-    respond_to do |format|
-      if @challenge.update_attributes(params[:challenge])
-        format.html { redirect_to @challenge, :notice => 'Challenge was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @challenge.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /challenges/1
-  # DELETE /challenges/1.json
-  def destroy
-    @challenge = Challenge.find(params[:id])
-    @challenge.destroy
-
-    respond_to do |format|
-      format.html { redirect_to challenges_url }
-      format.json { head :no_content }
-    end
   end
 
   private
